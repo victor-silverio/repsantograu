95405b12d0e760233037da4399d58ce8702700ae
Victor Augusto De Aquino Silvério <victoraugusto4096@outlook.com>
2025-11-27 11:33:07 -0300

    atualizações

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"], // Isso diz pro Tailwind olhar seu HTML para saber o que compilar
+  content: ["./**/*.{html,js}", "!./node_modules/**/*"], 
   theme: {
     extend: {
       colors: {
         repRed: '#8B0000',
         repGold: '#D4AF37',
         repWhite: '#FAFAFA',
-        repGrey: '#333333'
+        repGrey: '#333333',
+        whatsapp: '#128C7E',
+        whatsappDark: '#075E54',
       },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
